dced4b3e65320423ca959168fd1965615c1e9c6e
hjy22 <jhuang9768@gmail.com>
2020-02-21 22:11:27 +0800

    checkbox and textfield update

--- src/components/FeedbackSheet.vue
+++ src/components/FeedbackSheet.vue
@@ -15,66 +15,101 @@
             <v-btn dark text @click="dialog = false">Save</v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <v-list three-line subheader>
-          <v-subheader>User Controls</v-subheader>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Content filtering</v-list-item-title>
-              <v-list-item-subtitle>Set the content filtering level to restrict apps that can be downloaded</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Password</v-list-item-title>
-              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
         <v-divider></v-divider>
-        <v-list three-line subheader>
-          <v-subheader>General</v-subheader>
-          <v-list-item>
+        <v-row>
+            <v-col>
+                <v-container>
+            <v-list three-line subheader>
+          <v-subheader>Rating</v-subheader>
+          <v-flex  v-for="(checkBox, id) in checkBoxInfo" :key="id">
+              <v-list-item>
             <v-list-item-action>
-              <v-checkbox v-model="notifications"></v-checkbox>
+              <v-checkbox v-model="checkBox.model"></v-checkbox>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
-              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+              <v-list-item-title>{{checkBox.name}}</v-list-item-title>
+              <v-list-item-subtitle>{{checkBox.detail}}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="sound"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Sound</v-list-item-title>
-              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="widgets"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Auto-add widgets</v-list-item-title>
-              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+          </v-flex>
         </v-list>
+        </v-container>
+            </v-col>
+            <v-col>
+            <v-list three-line subheader>
+            <v-subheader>TextField</v-subheader>
+            <v-flex v-for="(text, id) in textFieldInfo" :key="id">
+                <v-list-item>
+                <v-list-item-action>
+                    <v-checkbox></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                        <v-list-item-subtitle>
+                            <v-textarea
+                            outlined
+                            disabled
+                            :label="text.label"
+                            rows="2"
+                            row-height="20"
+                            ></v-textarea>
+                        </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+            </v-flex>
+              <!-- <v-list-item>
+                <v-list-item-action>
+                    <v-checkbox v-model="organisation"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                        <v-list-item-subtitle>
+                            <v-textarea
+                            outlined
+                            disabled="true"
+                            label="Summarise briefly the question you asked"
+                            rows="2"
+                            row-height="20"
+                            ></v-textarea>
+                        </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item> -->
+        </v-list>
+        </v-col>
+        </v-row>
       </v-card>
     </v-dialog>
   </v-row>
 </template>
 
 <script>
+import axios from 'axios'
   export default {
     data () {
       return {
+        checkBoxInfo:[],
+        textFieldInfo:[],
         dialog: false,
-        notifications: false,
-        sound: true,
-        widgets: false,
+        organisation: false,
+        content: false,
+        presentation: false,
+        engagement:false,
+        delivery:false,
+        pace:false,
+        question:false,
       }
     },
+    created () {
+      this.getInfo() // 本地JSON
+    },
+     methods:{
+       getInfo() {
+        axios.get('../static/FeedbackSheet.json').then(response => {
+            this.checkBoxInfo = response.data.checkBox;
+            this.textFieldInfo = response.data.textField;
+            // console.log(this.checkBoxInfo);
+        }, response => {
+            console.log("error");
+        });
+      },
+     }
   }
 </script>
